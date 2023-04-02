a6991f7c3b2ce91faa5ceb179d3dfcd723e9c905
mohammad <aliakbariiimohammad@gmail.com>
2023-04-03 01:09:22 +0430

    add async function for fetch data and show that in #fact div

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
-let form = document.querySelector('form')
+const  form = document.querySelector('form')
+const  fact = document.querySelector('#fact')
+
+
+async function getFactYear (year='random') {
+    year = year ? year: 'random'
+    let  response =  await fetch(`http://numbersapi.com/${year}/year/`)
+    let data = await response.text()
+     fact.textContent =  data
+     fact .className += 'alert alert-success' 
+}
+
+
 form.addEventListener('submit', e=> {
     e.preventDefault()
+        getFactYear(e.target.year.value)
 })
